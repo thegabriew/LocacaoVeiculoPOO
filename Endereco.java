895442a37b934c7a9e8436f40e036ef641a4772e
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-11-07 18:53:19 -0300

    Update Endereco.java

--- Endereco.java
+++ Endereco.java
@@ -1,8 +1,16 @@
 
 public class Endereco {
-	String rua;
-	int numero;
-	String bairro;
-	String cidade;
+  String rua;
+  int numero;
+  String bairro;
+  String cidade;
 
+  public void cadastrarEnd(String r, int n, String bai, String cid) {
+
+    this.rua = r;
+    this.numero = n;
+    this.bairro = bai;
+    this.cidade = cid;
+
+  }
 }
