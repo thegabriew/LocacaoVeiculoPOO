5912765c6802e9436db89576bf99c57c3aeda4f0
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-11-13 09:43:15 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -4,4 +4,21 @@ public class Cliente {
 	String cpf;
 	String endereco;
 	int telefone;
+  Endereco end = new Endereco ();
+
+  public void cadastrarCliente(String n, String cpf, String end, int tel){
+    this.nome = n;
+    this.cpf = cpf;
+    this.endereco = end;
+    this.telefone = tel;
+}
+
+  public void exibirCLiente(){
+    System.out.println("Nome: " + this.nome);
+    System.out.println("CPF: " + this.cpf);
+    System.out.println("Endereço: " + this.endereco);
+    System.out.println("Telefone: " + this.telefone); 
+    end.exibirEnd();
+    
+  }
 }
