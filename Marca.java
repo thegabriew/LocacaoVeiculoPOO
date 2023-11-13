5594e2065383af429810f984f52ba7ec8b730b83
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-11-13 09:44:34 -0300

    Update Marca.java

--- Marca.java
+++ Marca.java
@@ -1,7 +1,18 @@
 
 public class Marca {
-	
-	int cod;
-	String desc;
-	
+
+  int cod;
+  String desc;
+
+  public void cadastrarMarca(int cod, String desc) {
+    this.cod = codigo;
+    this.desc = descricao;
+
+  }
+
+  public void exibirMarca() {
+    System.out.println("Sobre a marca: ");
+    System.out.println("Código: " + this.cod);
+    System.out.println("Descrição: " + this.desc);
+  }
 }
