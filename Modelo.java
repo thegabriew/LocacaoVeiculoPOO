de4d83ee42cb20ac15ea0bfe9cd05392109ad9ec
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-11-13 09:44:51 -0300

    Update Modelo.java

--- Modelo.java
+++ Modelo.java
@@ -1,8 +1,18 @@
 
 public class Modelo {
 
-	int cod;
-	String desc;
-	
-	
+  int cod;
+  String desc;
+
+  public void cadastrarModelo(int cod, String desc) {
+    this.cod = codigo;
+    this.desc = descricao;
+
+  }
+
+  public void exibirModelo() {
+    System.out.println("Sobre o modelo: ");
+    System.out.println("Código: " + this.cod);
+    System.out.println("Descrição: " + this.desc);
+  }
 }
